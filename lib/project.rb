10f6be03762f797ff5fa3292061b4b5b9a18233f
oliburt <oliburt@gmail.com>
2019-08-08 15:55:57 +0100

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,23 @@
+class Project
+    attr_reader :title
+    attr_accessor :backers
+
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        @backers = []
+        @@all << self
+    end
+
+    def add_backer(backer)
+        @backers << backer
+        backer.backed_projects << self
+    end
+
+
+    def self.all
+        @@allit
+    end
+
+end
